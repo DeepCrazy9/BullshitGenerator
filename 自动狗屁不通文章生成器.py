278232a226f3b24b24327bd0dc630ddcf9f56267
DeepCrazy9 <59865122+DeepCrazy9@users.noreply.github.com>
2020-01-15 09:01:45 +0900

    Update 自动狗屁不通文章生成器.py
    修复了判定问题。

--- 自动狗屁不通文章生成器.py
+++ 自动狗屁不通文章生成器.py
@@ -17,10 +17,9 @@ def generator(title, length):
 
     while len(body) < length:
         num = random.randint(0, 100)
-        if num < 10:
-            temp = random.choice(data["famous"]) \
-                .replace('a', random.choice(data["before"])) \
-                .replace('b', random.choice(data['after'])) + "\r\n"  # 解决了一句话没写完就换行的问题。比如bosh句子后有时候会直接连上一个换行。
+        a = textfilter[-1]
+        if num < 10 and a not in data["bosh"]:
+            body += "\r\n"  # 解决了一句话没写完就换行的问题。比如bosh句子后有时候会直接连上一个换行。
         elif num < 20:
             temp = random.choice(data["famous"]) \
                 .replace('a', random.choice(data["before"])) \
@@ -29,19 +28,13 @@ def generator(title, length):
             temp = random.choice(data["bosh"])
 
         while len(textfilter) < norepeat:
-            for i in textfilter:
-                if i == "":
-                    i = temp
-                    body += temp
-                elif i != temp:
-                    textfilter.append(temp)
-                    body += temp
-                else:
-                    break
-            break
+            if temp not in textfilter:
+                textfilter.append(temp)
+                body += temp
+            else:
+                break
         else:
             textfilter.pop(0)
-            continue
 
         body = body.replace("x", title)
 
